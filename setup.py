3b695a47c6665b5aa3e210dd22cc9402b490b5fc
Tuxedosam <mx666otp@gmail.com>
2024-09-28 16:56:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
 
+# -*- coding: utf-8 -*-
+
+setup(
+    name='Tuxedosam',
+    version="2.1",
+    packages=find_packages(),
+    author="Trey",
+    author_email="flawedmx@protonmail.com",
+    install_requires=["termcolor","bs4","httpx","trio","tqdm","colorama"],
+    description="Tuxedosam allows you to check if the mail is used on different sites like twitter, instagram , snapchat and will retrieve information on sites with the forgotten password function.",
+    include_package_data=True,
+    url='https://github.com/RedLatto/tuxedosam',
+    entry_points = {'console_scripts': ['RedLatto = RedLatto.core:main']},
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
